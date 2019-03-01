0708c2944036f69105ddcbbc3e8da736bf1fd9f1
timothé <timothe.laude@gmail.com>
2019-03-01 21:44:37 +0100

    concessionnaire en objet

--- tp8/src/tp8/Voiture.java
+++ tp8/src/tp8/Voiture.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Voiture {
 	private String marque;
 	private String type;
-	public static String[] voitur;
+	public static String[] voiture;
 	public Voiture() {
 		this.marque="rien";
 		this.type="rien";
@@ -32,14 +32,59 @@ public class Voiture {
 		
 		
 			
-		}for(int i=0;i<stock.size();i++) {
-			System.out.print(stock.get(i).marque+" ");
-			System.out.println(stock.get(i).type);
+			
+		}
+		while(true) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Que voulez vous?ajouter au stock (1) regarder stock (2) supprimer du stock (3) quitter(4): ");
+			int client=sc.nextInt();
+			if(client==1) {
+				ajouter(stock);
+				continue;
+			}else if(client==2) {
+				afficherStock(stock);
+				continue;
+			}else if(client==3) {
+				supprimer(stock);
+				continue;
+			}else if(client==4) {
+				System.out.println("a bientot");
+				break;
+			}
+		}
+	
+		
+		
+		
+		
+	}
+	public static void afficherStock(ArrayList<Voiture> tab) {
+		for(int i=0;i<tab.size();i++) {
+			System.out.print(tab.get(i).marque+" ");
+			System.out.println(tab.get(i).type);
 		
 		}
-	}
-	public static void afficherStock(Voiture[] tab) {
 		
+	}
+	public static void ajouter(ArrayList<Voiture> tab) {
+			Scanner ajm=new Scanner(System.in);
+			System.out.println("Rentrer la marque de la voiture :");
+			String marqu =ajm.nextLine();
+			Scanner ajt=new Scanner(System.in);
+			System.out.println("Rentre le type de la voiture "+marqu+": ");
+			String typ=ajt.nextLine();
+			tab.add(new Voiture(marqu,typ));
+			
+
+	}
+	public static void supprimer(ArrayList<Voiture> tab) {
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<tab.size();i++) {
+			System.out.println((i+1)+"-"+tab.get(i).marque+" "+tab.get(i).type);
+		}
+		System.out.println("Quel voiture avez vous vendu ? ");
+		int vendre=sc.nextInt();
+		tab.remove(tab.get(vendre-1));
 	}
 
 }
